4428ace91dfa247d0a888c6c55f57125f40222fb
lizakapranova <liza.a.kapranova@gmail.com>
2024-11-03 21:38:15 +0300

    Upload task_0

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -33,6 +33,53 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(num1, num2):
+    return num1 * num2
+
+def id(smth):
+    return smth
+
+def add(num1, num2):
+    return num1 + num2
+
+def neg(num):
+    return -num
+
+def lt(num1, num2):
+    return num1 < num2
+
+def eq(num1, num2):
+    return num1 == num2
+
+def max(num1, num2):
+    return num1 if num1 > num2 else num2
+
+def is_close(num1, num2):
+    return abs(num1 - num2) < 1e-2
+
+def sigmoid(num):
+    return 1.0 / (1.0 + exp(-num)) if num >= 0 else exp(num) / (1.0 + exp(num))
+
+def relu(num):
+    return max(0, num)
+
+def log(num):
+    return math.log(num)
+
+def exp(num):
+    return math.exp(num)
+
+def inv(num):
+    return 1.0 / num
+
+def log_back(num1, num2):
+    return (1.0 / num1) * num2
+
+def inv_back(num1, num2):
+    return (-1.0 / (num1 * num1)) * num2
+
+def relu_back(num1, num2):
+    return num2 if num1 > 0 else 0
 
 
 # ## Task 0.3
@@ -51,4 +98,29 @@ from typing import Callable, Iterable
 # - prod: take the product of lists
 
 
-# TODO: Implement for Task 0.3.
+def map(func: Callable, items: Iterable):
+    return [func(item) for item in items]
+
+def zipWith(func: Callable, items1: Iterable, items2: Iterable):
+    return [func(item1, item2) for item1, item2, in zip(items1, items2)]
+
+def reduce(func: Callable, items: Iterable):
+    result = None
+    for item in items:
+        if result is None:
+            result = item
+        else:
+            result = func(result, item)
+    return result or 0
+
+def negList(items: Iterable):
+    return map(neg, items)
+
+def addLists(items1: Iterable, items2: Iterable):
+    return zipWith(add, items1, items2)
+
+def sum(items: Iterable):
+    return reduce(add, items)
+
+def prod(items: Iterable):
+    return reduce(mul, items)
